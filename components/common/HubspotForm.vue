f225c695159a42295d686433e14f0154a0681b53
Drailan John Terrible <drailan@thebluerock.com.au>
2023-11-08 13:20:03 +0800

    feat: add hubspot form
    feat: initial components on home screen

---
+++ components/common/HubspotForm.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  mounted() {
+    const script = document.createElement("script");
+    script.src = "https://js.hsforms.net/forms/v2.js";
+    document.body.appendChild(script);
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "43352783",
+          formId: "4858e820-e1fa-4902-9268-883dc2c69501",
+          target: "#hubspotForm",
+        });
+      }
+    });
+  },
+};
+</script>
+<template>
+  <div id="hubspotForm" v-once></div>
+</template>
