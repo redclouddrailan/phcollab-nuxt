9a519ee31b1e6fb81d09db2bbaf9ed7845334d7d
alicanteallan <allan@exaweb.com.ph>
2023-11-09 10:44:09 +0800

    feat: Added Hero Text Component

---
+++ components/common/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+ <!-- logo component -->
+    <div class="w-60 grid justify-center items-center grid-cols-3 gap-3">
+      <div class="col-span-1">
+        <Image :src="logo" />
+      </div>
+      <div class="col-span-2">
+        <Image :src="logoText" alt="" />
+      </div>
+    </div>
+  <div class="">
+    <div class="text-7xl mt-20">{{ props.title }}</div>
+      <p class="text-lg mt-20">{{ props.description }}</p>
+  </div>
+</template>
+
+<script setup>
+  import logo from "/assets/images/logo.png";
+  import logoText from "/assets/images/logo-text.png";
+  const props = defineProps(["title", "description"]);
+</script>
