5a196cd311c500aa7873d1b4710e4e603fede8e7
alicanteallan <allan@exaweb.com.ph>
2023-11-09 10:44:35 +0800

    feat: added Tagline component

---
+++ components/common/Tagline.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="[writing-mode:vertical-rl] rotate-180 col-span-1 flex justify-center h-100"
+  >
+    <div class="mb-16">Code</div>
+    <div class="mb-16">Collaborate</div>
+    <div class="mb-16">Create</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
